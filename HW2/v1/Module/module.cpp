636279d833fabc011ec0c45bf93fb633dfc7160e
Frank <frank840629@gmail.com>
2022-10-24 23:25:33 +0800

    Add ListNode, BucketList, Set

--- HW2/v1/Module/module.cpp
+++ HW2/v1/Module/module.cpp
@@ -1,6 +1,9 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 class Net;
@@ -8,13 +11,13 @@ class Cell
 {
   public:
     string name;
-    int size_a, size_b;
+    vector<int> cellArea;
     int set;
     int gain;
     bool locked;
     vector<Net*> nets;
 
-    Cell(string name, int size_a, int size_b):name(name), size_a(size_a), size_b(size_b), set(0), locked(false) {}
+    Cell(string name, vector<int> cellArea):name(name), cellArea(cellArea), set(-1), locked(false) {}
 };
 
 class Net
@@ -22,17 +25,47 @@ class Net
   public:
     string name;
     vector<Cell*> cells; 
-    pair<int, int> setCnt;
+    vector<int> setCellCnt;
 
-    Net(string name):name(name), setCnt(make_pair(0,0)) {}
+    Net(string name):name(name), setCellCnt({0,0}) {}
 
+};
+
+class ListNode
+{
+  Cell* cell;
+  ListNode* next, *before;
+  ListNode(Cell* cell):cell(cell), next(nullptr), before(nullptr) {}
 };
 
 class BucketList
 {
   public:
     int size;
-    unordered_map<int, vector<Cell*>> bucketlist;
+    unordered_map<int, ListNode*> bucketlist;
 
     BucketList():size(0) {}
 };
+
+class Set
+{
+  public:
+    int area;
+    unordered_set<Cell*> cells;
+
+    Set() {}
+    void addCell(Cell* cell, int num); 
+    void deleteCell(Cell* cell, int num);
+};
+
+void Set::addCell(Cell* cell, int num)
+{
+  cells.insert(cell);
+  area += cell->cellArea[num];
+}
+
+void Set::deleteCell(Cell* cell, int num)
+{
+  cells.erase(cell);
+  area -= cell->cellArea[num];
+}
