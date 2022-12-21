3eb28854a93e6796967f1ad8c3c773c2db2ab321
frank <frank840629@gmail.com>
2022-12-21 15:18:59 +0800

    Revise via recursion

---
+++ HW4_Placement_Legalization/src/recursive.cpp
@@ -0,0 +1,54 @@
+void Legalizer::placeRowFinal(int const &rowIdx, int const &subRowIdx, Cell *cell)
+{
+	auto subRow = input->rows[rowIdx]->subRows[subRowIdx];
+	subRow->freeWidth -= cell->width;
+
+	double optimalX = cell->x;
+	if (cell->x < subRow->minX)
+		optimalX = subRow->minX;
+	else if (cell->x > subRow->maxX - cell->width)
+		optimalX = subRow->maxX - cell->width;
+
+	auto cluster = subRow->lastCluster;
+	if (cluster == nullptr || cluster->x + cluster->width <= optimalX)
+	{
+		cluster = new Cluster(optimalX,
+													subRow->lastCluster,
+													0,
+													0.0,
+													0);
+		addCell(cluster, cell, optimalX);
+		subRow->lastCluster = cluster;
+		// cluster->member.emplace_back(cell);
+	}
+	else
+	{
+		addCell(cluster, cell, optimalX);
+		while (true)
+		{
+			cluster->x = cluster->q / cluster->weight;
+
+			if (cluster->x < subRow->minX)
+					cluster->x = subRow->minX;
+			if (cluster->x > subRow->maxX - cluster->width)
+					cluster->x = subRow->maxX - cluster->width;
+
+			auto prevCluster = cluster->predecessor;
+			if (prevCluster != nullptr && prevCluster->x + prevCluster->width > cluster->x)
+			{
+					prevCluster->member.insert(prevCluster->member.end(), cluster->member.begin(), cluster->member.end());
+					prevCluster->weight += cluster->weight;
+					prevCluster->q += cluster->q - cluster->weight * prevCluster->width;
+					prevCluster->width += cluster->width;
+
+					delete cluster;
+					cluster = prevCluster;
+			}
+			else
+			{
+					break;
+			}
+		}
+		subRow->lastCluster = cluster;
+	}
+}
